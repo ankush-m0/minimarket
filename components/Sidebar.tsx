891d54a36bbc7d9f275a90c0324da0f950d192f3
nitesh-m0 <nitesh@m0.ventures>
2024-01-17 20:05:02 +0530

    added minimarket in sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,59 +10,71 @@ import NftSwap2 from "../images/NftSwap2.png";
 import Rewards from "../images/Rewards.png";
 import Settings from "../images/Settings.png";
 import Logo from "./logo/Logo";
+import Link from "next/link";
+import cart from "../images/cart.png";
 
 export default function Sidebar() {
   return (
     <div className="flex flex-col ">
-      <div className="border-b-2 border-black py-3 md:py-4">
-      <Logo />
+      <div className="md:border-b-2  border-black py-3 md:py-4">
+        <Logo />
       </div>
-      <div className="w-3/4 mx-auto">
-        <div className="flex flex-col border-b border-black items-center">
-          <label className="text-center font-bold my-5">SWAPS</label>
-          <div className="flex flex-col items-center mb-6 ">
+      <div className="w-3/4 mx-auto text-center space-y-5 ">
+        <div className=" border-b border-black flex md:hidden flex-col justify-center py-5 ">
+          <Image
+            src={cart}
+            alt="cart"
+            width={40}
+            height={40}
+            className="w-8 mx-auto"
+          />
+          <Link href={"/minimarket"} className="text-sm">
+            Mini-Market
+          </Link>
+        </div>
+        <div className="flex flex-col border-b border-black items-center gap-5 pb-5">
+          <label className="text-center font-bold">SWAPS</label>
+          <div className="flex flex-col items-center ">
             <Image src={DOTCSwap} alt="DotcSwap" width={30} height={30} />
             <button className="text-sm">Direct OTC Swap</button>
           </div>
-          <div className="flex flex-col items-center mb-6">
+          <div className="flex flex-col items-center">
             <Image src={COTCSwap} alt="DotcSwap" width={30} height={30} />
             <button className="text-sm">Cross-Chain OTC Swap</button>
           </div>
-          <div className="flex flex-col items-center mb-6">
+          <div className="flex flex-col items-center">
             <Image src={NFTSwap} alt="DotcSwap" width={30} height={30} />
             <button className="text-sm">NFT Swap</button>
           </div>
-          <div className="flex flex-col items-center mb-6">
+          <div className="flex flex-col items-center">
             <Image src={DexSwap} alt="DotcSwap" width={30} height={30} />
             <button className="text-sm">Dex Swap</button>
           </div>
         </div>
 
-        <div className="flex flex-col border-b border-black items-center">
-          <button className="text-center font-bold my-5">ALL TRADES</button>
-          <div className="flex flex-col items-center mb-6 ">
+        <div className="flex flex-col border-b border-black items-center gap-5 pb-5">
+          <button className="text-center font-bold">ALL TRADES</button>
+          <div className="flex flex-col items-center ">
             <Image src={DNSwap} alt="DotcSwap" width={30} height={30} />
             <button className="text-sm">Direct Normal Swap</button>
           </div>
-          <div className="flex flex-col items-center mb-6">
+          <div className="flex flex-col items-center">
             <Image src={CCSwap} alt="DotcSwap" width={30} height={30} />
             <button className="text-sm">Cross-Chain Swap</button>
           </div>
-          <div className="flex flex-col items-center mb-6">
+          <div className="flex flex-col items-center">
             <Image src={NftSwap2} alt="DotcSwap" width={30} height={30} />
             <button className="text-sm">NFT Swap</button>
           </div>
         </div>
 
-        <div className="flex flex-col border-b border-black items-center mb-6">
-          <button className="text-center font-bold my-5">
-            REWARDS & SETTINGS
-          </button>
-          <div className="flex flex-col items-center mb-6 ">
+        <div className="flex flex-col border-b border-black items-center gap-5 pb-5">
+          <button className="text-center font-bold">REWARDS & SETTINGS</button>
+          <div className="flex flex-col items-center  ">
             <Image src={Rewards} alt="DotcSwap" width={30} height={30} />
             <button className="text-sm">Rewards</button>
           </div>
-          <div className="flex flex-col items-center mb-6">
+          <div className="flex flex-col items-center ">
             <Image src={Settings} alt="DotcSwap" width={30} height={30} />
             <button className="text-sm">Settings</button>
           </div>
